235a77f77571ac9f6cc0d490129d9a71d189cef8
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-07-15 13:20:30 +0200

    Update config to enable assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental:{
+    assets: true,
+  },
   integrations: [tailwind(), sitemap(), compress()],
   site: 'https://log.consulting'
 });
